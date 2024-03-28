c1576a0f82cca9a0de5e4da3814f080b95b5d2a1
unknown <69082025+og-JND@users.noreply.github.com>
2024-03-28 13:12:11 -0400

    add file and ifstrem to read the file

--- main.cpp
+++ main.cpp
@@ -3,14 +3,23 @@ Okay so how am I gonna do this?
 first i need to accept command line parameters
 */
 #include <iostream>
+#include <fstream>
 using namespace std;
 
 int main(int argc, char const *argv[])
 {
-    for (size_t i = 0; i < argc; i++)
+    ifstream file("testfile.txt");
+    string line;
+    while (!file.eof())
     {
-        cout<<"argv: "<<argv[i]<<endl;
+        file>>line;
+        cout<<line<<endl;
     }
     
+    // for (size_t i = 0; i < argc; i++)
+    // {
+    //     cout<<"argv: "<<argv[i]<<endl;
+    // }
+    file.close();
     return 0;
 }
